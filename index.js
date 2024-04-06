c22250691cdd78f183489dda47f8adb426f7a72e
DivvSaxena <saxenadivv@gmail.com>
2024-04-06 08:10:55 +0530

    feature added

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+ /* FIREBASE */
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+ import { getDatabase ,ref , push as pushed , onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyCq2s22wohlUPu3lpnycQTo3aHVsNZUm1Y",
+    databaseURL: 'https://saathi-5c950-default-rtdb.asia-southeast1.firebasedatabase.app/',
+    authDomain: "saathi-5c950.firebaseapp.com",
+    projectId: "saathi-5c950",
+    storageBucket: "saathi-5c950.appspot.com",
+    messagingSenderId: "86230382615",
+    appId: "1:86230382615:web:955aa7a6b4cff29abd4452"
+  };
+
+
+  const app = initializeApp(firebaseConfig)
+  const database = getDatabase(app)
+  const usersInDB = ref(database,'users')
+
 // UI ELEMENTS
 
 
@@ -8,6 +27,7 @@ const aadanEl = document.getElementById('aadan')
 
 localStorage.setItem('balance','1892')
 
+let list = []
 
 let balance = JSON.parse(localStorage.getItem('balance'))
 
@@ -18,7 +38,10 @@ document.addEventListener('click', (e) => {
         if(balance >= amount){
             console.log(amount)
             localStorage.setItem('amount', `${amount}`)  
+            list.push({amount:`${amount}`,balance:`${balance - amount}`})
+            pushed(usersInDB,list)
             clearFields()
+            
         }else{
             alert('insufficent amount')
         }
--- khata.js
+++ khata.js
@@ -1,3 +1,24 @@
+ /* FIREBASE */
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+ import { getDatabase ,ref , push as pushed , onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyCq2s22wohlUPu3lpnycQTo3aHVsNZUm1Y",
+    databaseURL: 'https://saathi-5c950-default-rtdb.asia-southeast1.firebasedatabase.app/',
+    authDomain: "saathi-5c950.firebaseapp.com",
+    projectId: "saathi-5c950",
+    storageBucket: "saathi-5c950.appspot.com",
+    messagingSenderId: "86230382615",
+    appId: "1:86230382615:web:955aa7a6b4cff29abd4452"
+  };
+
+
+  const app = initializeApp(firebaseConfig)
+  const database = getDatabase(app)
+  const usersInDB = ref(database,'users')
+
+
+
 // KHATA ELEMENTS
 const tableBodyEl = document.getElementById('table-body')
 const currentBalanceEl = document.getElementById('current-balance')
@@ -9,20 +30,52 @@ let balance = JSON.parse(localStorage.getItem('balance'))
 let finalAmount = balance - transacted
 
 
-if(transacted){
-    tableBodyEl.innerHTML += `
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>${transacted}</td>
-                                    <td>${finalAmount}</td>
-                                </tr>
-                            </tbody>
-                         `
-    currentBalanceEl.textContent = `Rs ${finalAmount}`
+// if(transacted){
+//     tableBodyEl.innerHTML += `
+//                             <tbody>
+//                                 <tr>
+//                                     <td>1</td>
+//                                     <td>${transacted}</td>
+//                                     <td>${finalAmount}</td>
+//                                 </tr>
+//                             </tbody>
+//                          `
+//     currentBalanceEl.textContent = `Rs ${finalAmount}`
 
-    balance = finalAmount
+//     balance = finalAmount
+// }
+
+
+
+
+onValue(usersInDB , (snapshot) => {
+    let itemArray = Object.entries(snapshot.val())
+    let feedHtml = ''
+
+    
+
+    for(let item = itemArray.length - 1; item >= 0; item--){
+        let currentItem = itemArray[item][1]
+        
+        console.log(currentItem)
+        currentBalanceEl.textContent = `Rs ${currentItem[0].balance}`
+        feedHtml += getFeed(currentItem)
+        currentBalanceEl.textContent = `Rs ${currentItem[0].balance}`
+        
+    }
+    
+    render(feedHtml)
+})
+
+function getFeed(item){
+    return `<tr>
+                <td>.</td>
+                <td>${item[0].amount}</td>
+                <td>${item[0].balance}</td>
+            </tr>`
 }
 
-
-
+function render(feedHtml){
+    
+    tableBodyEl.innerHTML += feedHtml
+}
